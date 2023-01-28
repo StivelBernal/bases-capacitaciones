824b3d012660c0a59313b8914ab566d5d59e3b96
StivelBernal <brayan.bernalg@gmail.com>
2023-01-28 11:45:56 -0500

    Ajustes

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import './App.scss'
 
 function App () {
   return (
-    <div className='App'>
+    <div className='App' style={{ backgroundColor: '#24242437' }}>
       <h1>Hola mundo react</h1>
+      <h2>Hola mundo react 2</h2>
     </div>
   )
 }
